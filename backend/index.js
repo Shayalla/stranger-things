de0252f657496e9384615442a3804f3bb7dfd792
Jessie <miss_sc_jessi@hotmail.com>
2022-05-18 17:27:03 -0300

    push project

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+const { UPSIDEDOWN_MODE, PORT } = process.env;
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
